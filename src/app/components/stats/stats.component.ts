c642f30030c37dd77a8c756306bd0814832607bb
Viktors <vitja1234@gmail.com>
2021-05-03 11:46:43 +0300

    statistics directive

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -6,8 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit {
-  moneyLabels: [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`];
-  moneyDataLabels: [400, 300, 200, 160, 150, 100];
+  moneyLabels = [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`];
+  moneyDataLabels = [400, 300, 200, 160, 150, 100];
 
 
   constructor() {
